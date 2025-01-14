1261312c7b98eb0c9b9324ec14a3dd28d4cb582c
Tiolu <116491567+tioluu@users.noreply.github.com>
2025-01-14 21:23:23 +0100

    prettytable

--- src/coffee_machine_d16.py
+++ src/coffee_machine_d16.py
@@ -11,3 +11,7 @@ print(my_screen.canvheight)
 my_screen.exitonclick()
 
 table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle"])
+table.add_column("Type", ["Electric", "Water"])
+
+print(table)
