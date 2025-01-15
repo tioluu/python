16bc9bad27470fc09e47366b850f7b85a4a7a8e5
Tiolu <116491567+tioluu@users.noreply.github.com>
2025-01-15 15:47:04 +0100

    testing integration with code spaces

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+input("Good morning, what would you like? (espresso/latte/cappuccino): ")
