2f02262f2a043567bd90cf9e8d0466f84ff2cbb4
Tiolu <tholuu19@gmail.com>
2025-01-16 10:51:53 +0100

    implemented the first requirement

--- src/main.py
+++ src/main.py
@@ -2,4 +2,5 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-input("Good morning, what would you like? (espresso/latte/cappuccino): ")
+user_input = input("Good morning, what would you like? (espresso/latte/cappuccino): ")
+print(f"The user's input is {user_input}")
