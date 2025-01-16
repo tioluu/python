f5f587e192d5f853e2545fb12a4943e59ec47b95
Tiolu <116491567+tioluu@users.noreply.github.com>
2025-01-16 11:26:06 +0100

    Edited "test_src_files" file to Mock input()

--- tests/test_src_files.py
+++ tests/test_src_files.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 import sys
 import os
 import importlib
@@ -13,14 +14,17 @@ def test_coffee_maker_execution():
         assert False, f"Error in coffee_maker.py: {e}"
 
 def test_main_execution():
-    try:
-        # Add 'src' to the Python path to allow imports
-        sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-        
-        # Try importing 'main' to see if there are any import errors
-        import main
-    except Exception as e:
-        assert False, f"Error in main.py: {e}"
+    # Mock inputs for the program
+    inputs = ['espresso', 'off']  # Simulated user inputs
+    with patch('builtins.input', side_effect=inputs):
+        try:
+            # Add 'src' to the Python path to allow imports
+            sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+            
+            # Try importing 'main' to see if there are any import errors
+            import main
+        except Exception as e:
+            assert False, f"Error in main.py: {e}"
 
 def test_menu_execution():
     try:
