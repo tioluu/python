dac37d2cd71302c67b6ce43a9fae5ed0dc4f3d0a
Tiolu <116491567+tioluu@users.noreply.github.com>
2025-01-21 00:42:00 +0100

    changed main code

--- src/main.py
+++ src/main.py
@@ -2,9 +2,6 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-user_input = input("Good morning, what would you like? (espresso/latte/cappuccino): ")
-print(f"The user's input is {user_input}")
-
 menu = Menu()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
