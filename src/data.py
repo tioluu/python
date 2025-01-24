a6d9cec341fc0024911c630089006d9936dea9ac
Tiolu <116491567+tioluu@users.noreply.github.com>
2025-01-24 21:04:53 +0100

    fix linting

--- src/data.py
+++ src/data.py
@@ -1,7 +1,7 @@
 question_data = [
     {"text": "A slug's blood is green.", "answer": "True"},
     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", 
+    {"text": "Approximately one quarter of human bones are in the feet.",
     "answer": "True"},
     {"text": "The total surface area of a human lungs is the size of a "
     "football pitch.", "answer": "True"},
